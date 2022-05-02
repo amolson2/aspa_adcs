883053a4216df695bf264bb810e5733398e95b07
Matthew Coleman <msc5@princeton.edu>
2022-05-02 11:50:15 -0400

    plots

--- matlab/maneuvers.m
+++ matlab/maneuvers.m
@@ -6,15 +6,20 @@ addpath('matlab');
 [u, reg, quat] = util().reset();
 
 % Show plots?
-show_plots = true;
+show_plots = false;
 
 % Save plots to figures/?
-save_plots = false;
+save_plots = true;
 
-J = [3.1019279e+10  2.2047148e+07  2.2994735e+08;
-     2.2047148e+07  4.0036354e+10 -1.9779199e+07;
-     2.2994735e+08 -1.9779199e+07  3.9828543e+10] * 0.00142233;
+% CDR
+% J = [3.1019279e+10  2.2047148e+07  2.2994735e+08;
+%      2.2047148e+07  4.0036354e+10 -1.9779199e+07;
+%      2.2994735e+08 -1.9779199e+07  3.9828543e+10] * 0.00142233;
 
+% FDR
+J = [2.7938690e+10  3.2801419e+08  9.7966484e+08;
+    3.2801419e+08  3.5003171e+10 -2.8068004e+08;
+    9.7966484e+08 -2.8068004e+08  3.4081673e+10] * 0.00142233;
 
 %% Disturbance Torques (SRP, GG, etc.)
 
@@ -25,7 +30,7 @@ params = {};
 params.J = J;
 params.T = 20 * 60 * 60;
 params.M_ax = [0, 1, 0]';
-params.M = 1.10e-5;
+params.M = 6.722e-6;
 params.M_time = [];
 params.w_0 = [0; 0; 0];
 params.q_c = [0; 0; 0; 1];
@@ -54,14 +59,14 @@ u.show_plots(show_plots, plots);
 
 params = {};
 params.J = J;
-params.T = 20 * 60 * 60;
-params.M_ax = [0, 0, 1]';
+params.T = 1 * 60 * 60;
+params.M_ax = [-1, -1, 1]';
 params.M = 10;
 params.M_time = [0, 5];
 params.w_0 = [0; 0; 0];
 params.q_c = [0; 0; 0; 1];
-params.q_time = [1 * 60 * 60, params.T];
-params.k_p = 10; params.k_d = 1e5;
+params.q_time = [0, params.T];
+params.k_p = 0; params.k_d = 1e5;
 
 results = reg.regulate(params);
 wheels = reg.decompose(results);
@@ -85,13 +90,13 @@ u.show_plots(show_plots, plots);
 
 params = {};
 params.J = J;
-params.T = 16 * 60 * 60;
+params.T = 20 * 60 * 60;
 params.M = 0;
 params.w_0 = [0; 0; 0];
 e_hat = [0, 1, 0]';
 e_hat = e_hat / norm(e_hat);
 params.q_c = quat.q(e_hat, pi);
-params.k_p = 25; params.k_d = 1e5;
+params.k_p = 20; params.k_d = 1e5;
 
 results = reg.regulate(params);
 wheels = reg.decompose(results);
@@ -104,9 +109,66 @@ plots = [
     reg.plot_rotations(results);
 ];
 
-u.save_plots(save_plots, 'BellyFlop', plots);
+u.save_plots(save_plots, 'BellyFlop180', plots);
 u.show_plots(show_plots, plots);
 
+%% Thermal (30 degree turn)
+
+% Reset Workspace
+[u, reg, quat] = util().reset();
+
+params = {};
+params.J = J;
+params.T = 20 * 60 * 60;
+params.M = 0;
+params.w_0 = [0; 0; 0];
+e_hat = [0, 1, 0]';
+e_hat = e_hat / norm(e_hat);
+params.q_c = quat.q(e_hat, deg2rad(30));
+params.k_p = 20; params.k_d = 1e5;
+
+results = reg.regulate(params);
+wheels = reg.decompose(results);
+reg.analysis(results, wheels);
+
+plots = [
+    reg.plot_errors(results);
+    reg.plot_momenta(results);
+    reg.plot_wheel_momenta(wheels);
+    reg.plot_rotations(results);
+];
+
+u.save_plots(save_plots, 'BellyFlop30', plots);
+u.show_plots(show_plots, plots);
+
+%% Thermal (10 degree turn)
+
+% Reset Workspace
+[u, reg, quat] = util().reset();
+
+params = {};
+params.J = J;
+params.T = 20 * 60 * 60;
+params.M = 0;
+params.w_0 = [0; 0; 0];
+e_hat = [0, 1, 0]';
+e_hat = e_hat / norm(e_hat);
+params.q_c = quat.q(e_hat, deg2rad(10));
+params.k_p = 20; params.k_d = 1e5;
+
+results = reg.regulate(params);
+wheels = reg.decompose(results);
+reg.analysis(results, wheels);
+
+plots = [
+    reg.plot_errors(results);
+    reg.plot_momenta(results);
+    reg.plot_wheel_momenta(wheels);
+    reg.plot_rotations(results);
+];
+
+u.save_plots(save_plots, 'BellyFlop10', plots);
+u.show_plots(show_plots, plots);
 
 %% Thruster Misalignment
 
@@ -116,12 +178,14 @@ u.show_plots(show_plots, plots);
 params = {};
 params.J = J;
 params.T = 3 * 60 * 60;
-params.M = 5.52;
+% params.M = 5.52;
+params.M = 2;
 params.M_ax = [1, 1, 1]';
 params.M_time = [0, 90 * 60];
 params.w_0 = [0; 0; 0];
 params.q_c = [0; 0; 0; 1];
-params.k_p = 1e3; params.k_d = 1e5;
+% params.k_p = 1e3; params.k_d = 1e5;
+params.k_p = 5e2; params.k_d = 1e5;
 
 results = reg.regulate(params);
 wheels = reg.decompose(results);
@@ -134,7 +198,39 @@ plots = [
     reg.plot_rotations(results);
 ];
 
-u.save_plots(save_plots, 'ThrusterMisalignment', plots);
+u.save_plots(save_plots, 'ThrusterMisalignmentLong', plots);
+u.show_plots(show_plots, plots);
+
+
+%% Thruster Misalignment (Short)
+
+% Reset Workspace
+[u, reg, quat] = util().reset();
+
+params = {};
+params.J = J;
+params.T = 3 * 60 * 60;
+% params.M = 5.52;
+params.M = 2;
+params.M_ax = [1, 1, 1]';
+params.M_time = [0, 90 * 60];
+params.w_0 = [0; 0; 0];
+params.q_c = [0; 0; 0; 1];
+% params.k_p = 1e3; params.k_d = 1e5;
+params.k_p = 5e2; params.k_d = 1e5;
+
+results = reg.regulate(params);
+wheels = reg.decompose(results);
+reg.analysis(results, wheels);
+
+plots = [
+    reg.plot_errors(results);
+    reg.plot_momenta(results);
+    reg.plot_wheel_momenta(wheels);
+    reg.plot_rotations(results);
+];
+
+u.save_plots(save_plots, 'ThrusterMisalignmentShort', plots);
 u.show_plots(show_plots, plots);
 
 
@@ -155,12 +251,11 @@ params.M = 5.52;
 params.M_ax = [0, 1, 0]';
 params.M_time = [0, 100 * 60];
 params.error_time = params.M_time;
-% spinrate = 0.0089;
-spinrate = 0.0065;
+spinrate = 0.005;
 params.w_0 = [1; 0; 0] * spinrate;
 params.q_c = [0; 0; 0; 1];
 params.q_time = [100 * 60, params.T];
-params.k_p = 0; params.k_d = 5e3;
+params.k_p = 0.1; params.k_d = 500;
 
 results = reg.regulate(params);
 wheels = reg.decompose(results);
